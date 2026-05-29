675dee6f2d9e36bb2105ac8de7274706fdfd546b
Djállen Fabrício <djallendias@gmail.com>
2026-05-29 19:46:14 -0300

    Car class organization

--- ANNCarsGame/car.py
+++ ANNCarsGame/car.py
@@ -1,10 +1,12 @@
-
 class Car:
     def __init__(self, sprite, initial_xy: tuple[int, int], brain):
         self.sprite = sprite
         self.x, self.y = initial_xy
         self.angle = 0
+        self.alive = True
+        
         self.brain = brain
+        self.fitness = 0
         
     def update_car(self, x, y, angle):
         self.x += x
