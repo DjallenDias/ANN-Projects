83783b394ad33a9352d6b980c29ece98585201b1
Djállen Fabrício <djallendias@gmail.com>
2026-05-29 19:45:17 -0300

    Cars sprites now loaded in game

--- ANNCarsGame/game.py
+++ ANNCarsGame/game.py
@@ -1,9 +1,33 @@
+from random import choice
 import pygame
 from pygame.constants import HWSURFACE, DOUBLEBUF
+from pathlib import Path
+
+from car import Car
+from ann import ArtificialNeuralNetwork as ANN
+
+INPUT_NEURONS = 2
+HIDDEN_LAYERS = 2
+HIDDEN_NEURONS_PER_LAYER = 2
+OUTPUT_NEURONS = 2
+
+ANN_SIZES = [INPUT_NEURONS, HIDDEN_LAYERS, HIDDEN_NEURONS_PER_LAYER, OUTPUT_NEURONS]
+
+class GameState:
+    def __init__(self, qtyCars, sprites):
+        self.cars: list[Car] = []
+        for i in range(qtyCars):
+            self.cars.append(Car(choice(sprites), (0,0), ANN(*ANN_SIZES)))
 
 class Ui:
     def __init__(self):
         pygame.init()
+        
+        self.cars_dict = {}
+        for car_img_file in Path("assets/sprites/cars").glob("*_car.png"):
+            self.cars_dict[car_img_file.name] = pygame.image.load(car_img_file)
+        
+        self.gamestate = GameState(1, list(self.cars_dict.keys()))
         self.window = pygame.display.set_mode((1600, 870), HWSURFACE | DOUBLEBUF)
         
         self.info_area = self.window.subsurface(0, 0, 1600, 280)     
@@ -24,8 +48,23 @@ class Ui:
             if event.type == pygame.QUIT:
                 self.running = False
                 break
-            
+                
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_w]:
+            self.gamestate.cars[0].x += 1
+        
+        if keys[pygame.K_s]:
+            self.gamestate.cars[0].x -= 1
+    
+    def renderWorld(self, surface: pygame.Surface):
+        tileRect = pygame.Rect(0, 0, 64, 64)
+        for car in self.gamestate.cars:
+            surface.blit(self.cars_dict[car.sprite], (car.x, car.y), tileRect)
+    
     def render(self):
+        self.game_area.fill((0,255,0))
+        self.renderWorld(self.game_area)
         pygame.display.update()
         
     def run(self):
